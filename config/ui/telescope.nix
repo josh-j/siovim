eeef0d8f5daddb9e8627154c3c105782a0702f82
josh-j <joshj.tx@gmail.com>
2024-12-31 18:02:19 +0100

    a

--- config/ui/telescope.nix
+++ config/ui/telescope.nix
@@ -3,12 +3,33 @@
     enable = pkgs.lib.mkDefault true;
     settings = {
       defaults = {
-        path_display = ["truncate"];
         sorting_strategy = "ascending";
-        layout_config = {horizontal = {prompt_position = "top";};};
+        layout_strategy = "horizontal";
+        layout_config = {
+          horizontal = {
+            prompt_position = "top";
+            preview_width = 0.55;
+            results_width = 0.8;
+          };
+        };
+        vertical = {
+          mirror = false;
+        };
+        width = 0.87;
+        height = 0.80;
+        preview_cutoff = 120;
+        file_ignore_patterns = ["node_modules"];
+        #generic_sorter = require ("telescope.sorters").get_generic_fuzzy_sorter;
+        path_display = ["truncate"];
+        winblend = 0;
+        border = {};
+        borderchars = ["─" "│" "─" "│" "╭" "╮" "╯" "╰"];
+        color_devicons = true;
+        use_less = true;
+        set_env = {"COLORTERM" = "truecolor";};
       };
       pickers = {
-        find_files = {theme = "dropdown";};
+        find_files = {theme = "ivy";};
         git_files = {theme = "dropdown";};
         fd = {theme = "dropdown";};
         buffers = {theme = "dropdown";};
