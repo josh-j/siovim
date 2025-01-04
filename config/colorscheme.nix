5bc374fe52d96342214e60f659e88b1f8bf22712
josh-j <joshj.tx@gmail.com>
2025-01-05 00:23:40 +0100

    a

--- config/colorscheme.nix
+++ config/colorscheme.nix
@@ -1,89 +1,103 @@
 {pkgs, ...}: {
   colorschemes = {
-    #   palette = {
-    #     enable = pkgs.lib.mkDefault true;
-    #     settings = {
-    #     transparent_background = true;
-    #       custom_palettes = {
-    #         main = {
-    #           color0 = "#191d33";
-    #           color1 = "#1A1E39";
-    #           color2 = "#383f5e";
-    #           color3 = "#4e5470";
-    #           color4 = "#7b7f94";
-    #           color5 = "#a7a9b7";
-    #           color6 = "#bdbfc9";
-    #           color7 = "#d3d4db";
-    #           color8 = "#e9e9ed";
-    #         };
-    #         accent = {
-    #           accent0 = "#D97C8F";
-    #           accent1 = "#D9AE7E";
-    #           accent2 = "#D9D87E";
-    #           accent3 = "#A5D9A7";
-    #           accent4 = "#8BB9C8";
-    #           accent5 = "#C9A1D3";
-    #           accent6 = "#B8A1D9";
-    #         };
-    #         state = {
-    #           error = "#D97C8F";
-    #           warning = "#D9AE7E";
-    #           hint = "#D9D87E";
-    #           ok = "#A5D9A7";
-    #           info = "#8BB9C8";
-    #         };
-    #       };
-    #     };
-    #   };
-
-    cyberdream = {
-      # enable = pkgs.lib.mkDefault true;
-      enable = false;
+    palette = {
+      enable = pkgs.lib.mkDefault true;
       settings = {
-        transparent = true;
-        italic_comments = true;
-        hide_fillchars = true;
-        terminal_colors = false;
-
-        extensions = {
-          telescope = true;
-          notify = true;
-          mini = true;
-          cmp = true;
-          gitsigns = true;
-          dashboard = true;
-          indentblankline = true;
-          noice = true;
-          snacks = true;
-          treesitter = true;
-          treesittercontext = true;
-          trouble = true;
-          whichkey = true;
+        transparent_background = true;
+        palettes = {
+          main = "papermain";
+          accent = "paperaccent";
+          state = "paperstate";
         };
-
-        theme = {
-          colors = {
-            bg = "#1c1c1c";
-            bgAlt = "#1c1c1c";
-            bgHighlight = "#3c4048";
-            fg = "#bdbfc9";
-            grey = "#7b7f94";
-            blue = "#8BB9C8";
-            green = "#5faf5f";
-            cyan = "#00afaf";
-            red = "#af005f";
-            yellow = "#ffaf00";
-            magenta = "#af87d7";
-            pink = "#ff5faf";
-            orange = "#d7af5f";
-            purple = "#B8A1D9";
+        custom_palettes = {
+          main = {
+            papermain = {
+              color0 = "#993f33";
+              color1 = "#2c2c2c"; # selected line
+              color2 = "#2c2c2c"; #indentlines
+              # color3 = "#404050"; #linenumbers, folders
+              color3 = "#505060"; #linenumbers, folders
+              # color4 = "#ff5faf"; #highlighted line number
+              color4 = "#af87d7"; #highlighted line number
+              color5 = "#868086"; #varnames
+              color6 = "#9b9fb4"; #comments
+              color7 = "#009090";
+              color8 = "#606040"; # operators and strings
+            };
           };
-          variation = "dark";
-          # saturation = 0.7;
+          accent = {
+            paperaccent = {
+              accent0 = "#D97C8F";
+              accent1 = "#D9AE7E";
+              accent2 = "#D9D87E";
+              accent3 = "#A5D9A7";
+              # accent4 = "#8BB9C8";
+              accent4 = "#00afaf";
+              accent5 = "#C9A1D3";
+              accent6 = "#B8A1D9";
+            };
+          };
+          state = {
+            paperstate = {
+              error = "#D97C8F";
+              warning = "#D9AE7E";
+              hint = "#D9D87E";
+              ok = "#A5D9A7";
+              info = "#8BB9C8";
+            };
+          };
         };
       };
     };
-
+    #
+    # cyberdream = {
+    #   # enable = pkgs.lib.mkDefault true;
+    #   enable = false;
+    #   settings = {
+    #     transparent = true;
+    #     italic_comments = true;
+    #     hide_fillchars = true;
+    #     terminal_colors = false;
+    #
+    #     extensions = {
+    #       telescope = true;
+    #       notify = true;
+    #       mini = true;
+    #       cmp = true;
+    #       gitsigns = true;
+    #       dashboard = true;
+    #       indentblankline = true;
+    #       noice = true;
+    #       snacks = true;
+    #       treesitter = true;
+    #       treesittercontext = true;
+    #       trouble = true;
+    #       whichkey = true;
+    #     };
+    #
+    #     theme = {
+    #       colors = {
+    #         bg = "#1c1c1c";
+    #         bgAlt = "#1c1c1c";
+    #         bgHighlight = "#3c4048";
+    #         fg = "#bdbfc9";
+    #         grey = "#7b7f94";
+    #         blue = "#8BB9C8";
+    #         green = "#5faf5f";
+    #         cyan = "#00afaf";
+    #         red = "#af005f";
+    #         yellow = "#ffaf00";
+    #         magenta = "#af87d7";
+    #         pink = "#ff5faf";
+    #         orange = "#d7af5f";
+    #         purple = "#B8A1D9";
+    #       };
+    #       variation = "dark";
+    #       # saturation = 0.7;
+    #     };
+    #   };
+    # };
+    #
     oxocarbon = {
       enable = false;
     };
@@ -92,105 +106,24 @@
       enable = false;
       settings = {
         transparent_mode = true;
-        integrations = {
-          cmp = true;
-          flash = true;
-          fidget = true;
-          gitsigns = true;
-          indent_blankline.enabled = true;
-          lsp_trouble = true;
-          mini.enabled = true;
-          neotree = true;
-          noice = true;
-          notify = true;
-          telescope.enabled = true;
-          treesitter = true;
-          treesitter_context = true;
-          which_key = true;
-          native_lsp = {
-            enabled = true;
-            inlay_hints = {
-              background = true;
-            };
-            virtual_text = {
-              errors = ["italic"];
-              hints = ["italic"];
-              information = ["italic"];
-              warnings = ["italic"];
-              ok = ["italic"];
-            };
-            underlines = {
-              errors = ["underline"];
-              hints = ["underline"];
-              information = ["underline"];
-              warnings = ["underline"];
-            };
-          };
+        undercurl = true;
+        underline = true;
+        bold = true;
+        italic = {
+          strings = true;
+          emphasis = true;
+          comments = true;
+          operators = false;
+          folds = true;
         };
-      };
-    };
-  };
-
-  catppuccin = {
-       enable = pkgs.lib.mkDefault true;
-    settings = {
-      background = {
-        light = "macchiato";
-        dark = "mocha";
-      };
-      custom_highlights = ''
-        function(highlights)
-          return {
-          CursorLineNr = { fg = highlights.peach, style = {} },
-          NavicText = { fg = highlights.text },
-          }
-        end
-      '';
-      flavour = "macchiato"; # "latte", "mocha", "frappe", "macchiato" or raw lua code
-      no_bold = false;
-      no_italic = false;
-      no_underline = false;
-      transparent_background = true;
-      integrations = {
-        cmp = true;
-        notify = true;
-        gitsigns = true;
-        neotree = true;
-        which_key = true;
-        illuminate = {
-          enabled = true;
-          lsp = true;
-        };
-        navic = {
-          enabled = true;
-          custom_bg = "NONE";
-        };
-        treesitter = true;
-        telescope.enabled = true;
-        indent_blankline.enabled = true;
-        mini = {
-          enabled = true;
-          indentscope_color = "rosewater";
-        };
-        native_lsp = {
-          enabled = true;
-          inlay_hints = {
-            background = true;
-          };
-          virtual_text = {
-            errors = ["italic"];
-            hints = ["italic"];
-            information = ["italic"];
-            warnings = ["italic"];
-            ok = ["italic"];
-          };
-          underlines = {
-            errors = ["underline"];
-            hints = ["underline"];
-            information = ["underline"];
-            warnings = ["underline"];
-          };
-        };
+        strikethrough = true;
+        invert_selection = false;
+        invert_signs = false;
+        invert_tabline = false;
+        invert_intend_guides = false;
+        inverse = true; #-- invert background for search, diffs, statuslines and errors
+        contrast = "soft"; #-- can be "hard", "soft" or empty string
+        dim_inactive = false;
       };
     };
   };
