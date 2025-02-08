a7c48d56f40cf112db61ebfaeed29dee646b3e3c
josh-j <joshj.tx@gmail.com>
2025-02-08 22:05:43 +0100

    update config

--- config/lang/cpp.nix
+++ config/lang/cpp.nix
@@ -35,13 +35,13 @@
           require("clangd_extensions.inlay_hints").setup_autocmd()
           require("clangd_extensions.inlay_hints").set_inlay_hints()
         '';
-        extraOptions = {
-          init_options = {
-            usePlaceholders = true;
-            completeUnimported = true;
-            clangdFileStatus = true;
-          };
-        };
+        # extraOptions = {
+        #   init_options = {
+        #     usePlaceholders = true;
+        #     completeUnimported = true;
+        #     clangdFileStatus = true;
+        #   };
+        # };
       };
     };
 
