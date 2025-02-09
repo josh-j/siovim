f1e5f21c5e00b6f68225ea829955d72527f7022f
josh-j <joshj.tx@gmail.com>
2025-02-09 21:11:42 +0100

    update config

--- config/themes/oxocarbon.nix
+++ config/themes/oxocarbon.nix
@@ -1,16 +1,16 @@
-{
-  lib,
-  ...
-}:
-lib.nixvim.plugins.mkVimPlugin {
-  name = "oxocarbon";
-  isColorscheme = true;
-  packPathName = "oxocarbon.nvim";
-  package = "oxocarbon-nvim";
 
-  maintainers = [ lib.maintainers.GaetanLepage ];
-
-  extraConfig = {
-    opts.termguicolors = lib.mkDefault true;
-  };
+{ pkgs, ... }: {
+  programs.neovim.plugins = with pkgs.vimPlugins; [
+    # oxocarbon-theme
+    {
+      plugin = oxocarbon-nvim;
+      type = "lua";
+      config = /* lua */ ''
+        vim.opt.background = "dark" -- set this to dark or light
+        vim.cmd("colorscheme oxocarbon")
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+      '';
+    }
+  ];
 }
