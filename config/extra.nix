8196b8f3cea0fe5197d7829990bf63104813c3cb
josh-j <joshj.tx@gmail.com>
2025-02-12 20:20:09 +0100

    .

--- config/extra.nix
+++ config/extra.nix
@@ -1,15 +1,39 @@
 {pkgs, stablePkgs, ...}: {
-  extraConfigLuaPost = builtins.readFile ./extra.lua;
+  extraConfigLuaPost = ''
+    ${builtins.readFile ./extra.lua}
+    -- PowerShell LSP configuration
+    require('lspconfig').powershell_es.setup {
+      bundle_path = '${pkgs.powershell-editor-services}/lib/PowerShellEditorServices',
+      cmd = {
+        '${pkgs.powershell}/bin/pwsh',
+        '-NoLogo',
+        '-Command',
+        'Import-Module ${pkgs.powershell-editor-services}/PowerShellEditorServices/Start-EditorServices.ps1'
+      },
+      settings = {
+        powershell = {
+          codeFormatting = { Preset = "Custom" },
+          scriptAnalysis = { enable = true }
+        }
+      }
+    }
+  '';
 
   extraPackages = with pkgs; [
-    # runtime dependencies
     fd
     ripgrep
+    # powershell  # Required core component
     powershell-editor-services
+    nodejs  # Often required for LSP clients
+
+    # PowerShell dependencies
+    icu
+    openssl
+    zlib
   ] ++ [
-    stablePkgs.lldb  # Use LLDB from stable
-  ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-    # runtime dependencies
+    stablePkgs.lldb
+  ] ++ lib.optionals stdenv.isDarwin [
     gnused
+    darwin.libobjc
   ];
 }
