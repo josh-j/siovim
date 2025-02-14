4bc3d37ee17620712686be7c2c3e063b8b47e53d
josh-j <joshj.tx@gmail.com>
2025-02-14 20:46:09 +0100

    .

--- config/lsp/lsp.nix
+++ config/lsp/lsp.nix
@@ -1,64 +1,48 @@
+# lsp.nix
+{ pkgs, ... }:
+
 {
-  pkgs,
-  ...
-}: {
-  plugins = {
-    lsp = {
-      enable = true;  # Simplified: no need for pkgs.lib.mkDefault
-      servers.typos_lsp.enable = false; # Simplified
+  enable = true;
+  servers.typos_lsp.enable = false;
 
-      keymaps.lspBuf = {
-        "<c-k>" = "signature_help";
-        "gi" = "implementation";
-      };
+  keymaps.lspBuf = {
+    "<c-k>" = "signature_help";
+    "gi" = "implementation";
+  };
 
-      servers = {
-        # Add PowerShell LSP
-        powershell-editor-services = {
-          enable = true;
-          package = pkgs.powershell;  # Use the PowerShell package
-          settings = {
-            powershell = {
-              scriptAnalysis.enable = true;
-            };
-            powerShellEditorServices = {
-              powerShellExePath = "${pkgs.powershell}/bin/pwsh";
-            };
-            # Optional: Add more PowerShell-specific settings if desired
-            # codeFormatting = { ... };
-          };
-        };
-
-        # Existing LSP servers
-        bashls.enable = true;
-        dockerls.enable = true;
-        gopls.enable = true;
-        jsonls.enable = true;
-        marksman.enable = true;
-        nil_ls.enable = true;
-        pyright.enable = true;
-        ts_ls.enable = true;
-        lua_ls.enable = true;
-        tailwindcss.enable = true;
-        tinymist.enable = true;
-        cssls.enable = true;
-        html.enable = true;
-        htmx.enable = true;
-        solargraph.enable = true;
-        yamlls.enable = true;
-        taplo.enable = true;
+  servers = {
+    # PowerShell LSP Configuration (Manual)
+    powershell_es = {
+      enable = true;
+      package = pkgs.powershell; # Correct package
+      settings = {
+        powerShellExePath = "${pkgs.powershell}/bin/pwsh"; # Path to pwsh
+        # Other optional PowerShell settings can go here.
+        # For example, you *could* add script analysis:
+        # powershell = {
+        #   scriptAnalysis.enable = true;
+        # };
+        # But it's not strictly required for basic functionality.
       };
     };
 
-    lint.enable = true; # Simplified
+    # Other LSP servers (as before)
+    bashls.enable = true;
+    dockerls.enable = true;
+    gopls.enable = true;
+    jsonls.enable = true;
+    marksman.enable = true;
+    nil_ls.enable = true;
+    pyright.enable = true;
+    ts_ls.enable = true;
+    lua_ls.enable = true;
+    tailwindcss.enable = true;
+    tinymist.enable = true;
+    cssls.enable = true;
+    html.enable = true;
+    htmx.enable = true;
+    solargraph.enable = true;
+    yamlls.enable = true;
+    taplo.enable = true;
   };
-
-  keymaps = [
-    {
-      mode = "n";
-      key = "<leader>cl";
-      action = "<cmd>LspInfo<cr>";
-      options.desc = "Lsp Info";
-    }
-  ];
 }
