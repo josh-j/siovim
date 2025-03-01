bf8a1b5e59c4f6074c2982c8da519ad7bc36d900
josh-j <joshj.tx@gmail.com>
2025-03-01 11:44:30 +0100

    .

--- config/plugins/plugins-custom-vim.nix
+++ config/plugins/plugins-custom-vim.nix
@@ -35,7 +35,7 @@
 
   auto-dark-mode = pkgs.vimUtils.buildVimPlugin {
     pname = "auto-dark-mode-nvim";
-    src = pkgs.fetchgit {
+    src = pkgs.fetchFromGitHub {
       repo = "auto-dark-mode.nvim";
       owner = "f-person";
       rev = "2ef9553e2a1d6e861bc6955d58ce5883d28a6ad";
